3b5a5222139c82fc82f2b4f5569ec58604e757ae
Ege Emir Özkan <egeemirozkan24@gmail.com>
2021-07-29 20:02:09 +0300

    Fix bug related to undefined default state in the reducer.

--- tabs/src/rxutils/reducers/authReducer.ts
+++ tabs/src/rxutils/reducers/authReducer.ts
@@ -1,6 +1,11 @@
 import { User } from "../../api/User";
 import { authAction } from "../../interfaces/reduxActions";
 
-export default function authReducer(state: User = new User(false, ""), action: authAction) {
-  return action.newUser;
+export default function authReducer(state = new User(false, ""), action: authAction) {
+  switch (action.type) {
+    case "AUTH_SUCCESS":
+      return action.newUser;
+    default:
+      return state;
+  }
 }
